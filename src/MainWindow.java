07f367225590300d2c23bd9271c3f3b88928adbc
Vishal Pise <vishalpise095@gmail.com>
2024-04-23 23:34:03 +0530

    Implemented Save and Table Load

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -6,10 +6,12 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import net.proteanit.sql.DbUtils;
 import org.jdesktop.swingx.JXDatePicker;
+
 import java.util.Calendar;
 import java.util.Locale;
-import java.util.Objects;
 
 public class MainWindow {
     private JPanel mainPanel;
@@ -20,6 +22,7 @@ public class MainWindow {
     private JPanel mainPanelParent;
     private JTextField textField3;
     private JXDatePicker JXDatePicker1;
+    private JScrollPane table_1;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Kothrud Fitness");
@@ -44,57 +47,82 @@ public class MainWindow {
         }
     }
 
-    public java.sql.Date dateConversion(Date date){
+    public String dateConversion(Date date) {
         DateFormat inputFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);
         DateFormat outputFormat = new SimpleDateFormat("dd-MM-yyyy");
         Date utilDate = null;
+        String formattedDate = null;
         try {
             Date convertedDate = inputFormat.parse(date.toString());
-            String formattedDate = outputFormat.format(convertedDate);
+            formattedDate = outputFormat.format(convertedDate);
             System.out.println("Formatted date: " + formattedDate);
-            utilDate = outputFormat.parse(formattedDate);
+            //utilDate = outputFormat.parse(formattedDate);
 
         } catch (ParseException es) {
             es.printStackTrace();
         }
-        return new java.sql.Date(Objects.requireNonNull(utilDate).getTime());
+        return formattedDate;
+    }
+
+    void tableLoad() {
+        try {
+            pst = con.prepareStatement("select * from member_info");
+            ResultSet rs = pst.executeQuery();
+            table1.setModel(DbUtils.resultSetToTableModel(rs));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String addMonths(Date date, int days) {
+        DateFormat outputFormat = new SimpleDateFormat("dd-MM-yyyy");
+        System.out.println("Date ---" + date + " Days-----" + days);
+        Date utilDate = null;
+        String formattedDate = null;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_MONTH, days); // Add 3 months
+        utilDate = calendar.getTime();
+        formattedDate = outputFormat.format(utilDate);
+        System.out.println("New Date after adding 3 months: " + formattedDate);
+        return formattedDate;
     }
 
     public MainWindow() {
         connect();
+        tableLoad();
         JXDatePicker1.setDate(Calendar.getInstance().getTime());
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                String firstName, lastName;
-                java.sql.Date joiningDate;
-                double months;
+                String firstName, lastName, joiningDate, renewalDate;
+                int days;
                 firstName = textField1.getText();
                 lastName = textField2.getText();
-                joiningDate = dateConversion(JXDatePicker1.getDate());
-                months = Double.parseDouble(textField3.getText());
                 try {
-                    pst = con.prepareStatement("insert into member_info(first_name,last_name,date_of_joining)values(?,?,?)");
+                    joiningDate = dateConversion(JXDatePicker1.getDate());
+                    days = (int) (Double.parseDouble(textField3.getText()) * 30);
+                    renewalDate = addMonths(JXDatePicker1.getDate(), days);
+                    pst = con.prepareStatement("insert into member_info(first_name,last_name,date_of_joining,renewal_date)values(?,?,?,?)");
                     pst.setString(1, firstName);
                     pst.setString(2, lastName);
-                    pst.setDate(3, joiningDate);
+                    pst.setString(3, joiningDate);
+                    pst.setString(4, renewalDate);
                     pst.executeUpdate();
                     JOptionPane.showMessageDialog(null, "Record Addedddd!!!!!");
                     //table_load();
                     textField1.setText("");
                     textField2.setText("");
                     textField3.setText("");
+                    JXDatePicker1.setDate(new Date());
                     textField1.requestFocus();
+                    tableLoad();
+                } catch (Exception e1) {
+                    JOptionPane.showMessageDialog(null, "Check Entered Values!!!!");
                 }
-
-                catch (SQLException e1)
-                {
-
-                    e1.printStackTrace();
-                }
-
             }
         });
+
     }
 }
